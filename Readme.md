c238f5f4bad15552fcb0f93f3c258cd52003aa5b
souvikpramanikgit <souvikpramanik874@gmail.com>
2025-04-14 16:53:15 +0530

    Add Changes

---
+++ Readme.md
@@ -0,0 +1,11 @@
+<p align="center">
+  <img src="https://github.com/souvikpramanikgit/Souvik_web/blob/main/images/simage.png?raw=true" width='200' alt="Logo" >
+
+   <h3 align="center">CLI tool to display information about me</h3>
+</p>
+
+
+## To run this
+```bash
+npx souvik-web
+```
--- index.js
+++ index.js
@@ -1,51 +1,118 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
-import figlet from 'figlet';
+import chalk from "chalk";
+import figlet from "figlet";
+import boxen from "boxen";
+import gradient from "gradient-string";
 
-const generateText = (text, font) => {
-  return new Promise((resolve, reject) => {
-    figlet.text(text, {
-      font: font,
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
-    }, (err, data) => {
-      if (err) {
-        reject('Something went wrong...');
-      }
-      resolve(data);
-    });
-  });
-};
+const text1 = figlet.textSync("Hi, ", {
+  font: "Standard",
+  verticalLayout: "default",
+  horizontalLayout: "full",
+  width: 150,
+  whitespaceBreak: true,
+});
 
-(async () => {
-  try {
-    const hiThere = await generateText('HI THERE!!', 'ANSI Shadow');
-    const personalDetails = await generateText('PERSONAL DETAILS', 'ANSI Shadow');
-    const skills = await generateText('SKILLS', 'ANSI Shadow');
-    const message = await generateText('MESSAGE', 'ANSI Shadow');
 
-    console.log(chalk.magenta(hiThere));
+const text2 = figlet.textSync("I'm Souvik", {
+  font: "Standard",
+  verticalLayout: "default",
+  horizontalLayout: "full",
+  width: 150,
+  whitespaceBreak: true,
+});
 
-    console.log(chalk.green('✔ Loading Souvik\'s details\n'));
 
-    console.log(chalk.yellow.bold(personalDetails));
-    console.log(`${chalk.cyan.bold('NAME')}    - Souvik Kumar Pramanik`);
-    console.log(`${chalk.cyan.bold('STATUS')}  - Student`);
-    console.log(`${chalk.cyan.bold('GITHUB')}  - btw its awesome -> (https://github.com/souvikpramanikgit)`);
-    console.log(`${chalk.cyan.bold('WEBSITE')} - Coolest thing -> (https://souviks.netlify.app/)\n`);
+console.log(`
+  ${gradient(["cyan", "magenta"])("")}`);
 
-    console.log(chalk.yellow.bold(skills));
-    console.log(`${chalk.green.bold('LANGUAGES')}   - Java, C++, JavaScript`);
-    console.log(`${chalk.green.bold('FRAMEWORKS')}  - React`);
-    console.log(`${chalk.green.bold('TECHNOLOGIES')} - Express, Nodejs`);
-    console.log(`${chalk.green.bold('DATABASES')}   - MongoDB,  SQL`);
-    // console.log(`${chalk.green.bold('DEVOPS')}      - Docker, Kubernetes, Github Actions, AWS, AZURE`);
-    console.log(`${chalk.green.bold('TOOLS')}       - GIT, Github\n`);
+const gradientText1 = gradient(["cyan", "blue", "magenta"])(text1);
+console.log(gradientText1);
+const gradientText2 = gradient(["cyan", "blue", "magenta"])(text2);
+console.log(gradientText2);
 
-    // console.log(chalk.yellow.bold(message));
-    // console.log('Souvik is up for hire, ready to help your company get to next heights and ofc Thanks for checking out my cli!');
-  } catch (err) {
-    console.error(chalk.red(err));
-  }
-})();
+console.log(
+  chalk.cyanBright.bold(`
+  ✔ Loading Souvik's details ...
+  `)
+);
+
+const personelDetails = ` ${chalk.bold.whiteBright(
+  "Name")}             -         ${chalk.yellowBright("Souvik Kumar Pramanik")}
+ ${chalk.bold.white("Status")}           -         ${chalk.yellowBright("Student")}
+ ${chalk.bold.whiteBright("Github")}           -         ${chalk.yellowBright("https://github.com/souvikpramanikgit")}
+ ${chalk.bold.whiteBright("Portfolio")}        -         ${chalk.yellowBright(
+"https://souviktop.netlify.app/"
+)}`;
+
+console.log(
+  boxen(personelDetails, {
+    padding: 1,
+    margintop: 1,
+    borderStyle: "none",
+    backgroundColor: "#1e1e2f",
+    titleAlignment: "center",
+  })
+);
+
+const skillText = figlet.textSync("SKILLS", {
+  font: "Small",
+  horizontalLayout: "fitted",
+  width: "100",
+});
+const gradSkilltext = gradient(["#a8ff78", "#78ffd6"])(skillText);
+
+console.log(` \n${gradSkilltext}`);
+
+const skillsDetails = ` ${chalk.bold.whiteBright(
+  "Languages"
+)}        -         ${chalk.yellowBright(
+  "Java, C++, Python, Javascript"
+)}
+ ${chalk.bold.whiteBright("Frameworks")}       -         ${chalk.yellowBright(
+  "React, SpringBoot "
+)}
+ ${chalk.bold.whiteBright("Technologies")}     -         ${chalk.yellowBright(
+  "Express, Nodejs"
+)}
+ ${chalk.bold.whiteBright("Databases")}        -         ${chalk.yellowBright(
+  "MongoDb, MySQL"
+)}
+ ${chalk.bold.whiteBright("Others")}           -         ${chalk.yellowBright(
+  "Git, GitHub"
+)}`;
+
+console.log(
+  boxen(skillsDetails, {
+    padding: 1,
+    margin: 0,
+    borderStyle: "none",
+    backgroundColor: "#1e1e2f",
+    titleAlignment: "center",
+  })
+);
+
+const msgTxt = figlet.textSync("MESSAGE", {
+  font: "Small",
+  width: "100",
+  horizontalLayout: "fitted",
+});
+const gradMsgtxt = gradient(["#005AA7", "#FFFDE4"])(msgTxt);
+console.log(`\n${gradMsgtxt}`);
+
+const lastTxt = chalk.whiteBright(
+  `Souvik is open for opportunities, ready to bring value to
+your team and take things to the next level. Thanks for
+checking out my CLI!`
+);
+console.log(
+  boxen(lastTxt, {
+    padding: 1,
+    margin: 0,
+    borderStyle: "none",
+    backgroundColor: "#1e1e2f",
+    titleAlignment: "center",
+  })
+);
+console.log(`
+  ${gradient(["cyan", "magenta"])("")}`);
